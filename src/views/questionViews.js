ee226958f6250e8dacf7f1cab285e4c5ba29c51e
AlexanderSaid <alexander.t.said@gmail.com>
2021-11-12 07:35:14 +0100

    modified createQuestionElement() to add the references, and build createLinkElement()

--- src/views/questionViews.js
+++ src/views/questionViews.js
@@ -13,16 +13,24 @@ export const createAnswerElement = (answerText) => {
   answerElement.addEventListener('click', selectedAnswer);
   return answerElement;
 };
+export const createLinkElement = (linkText) => {
+  const linkElement = createDOMElement('a');
+  linkElement.innerText = linkText;
 
+  return linkElement;
+};
 /**
  * Create a full question element
  */
 export const createQuestionElement = (question) => {
+  // Create question container
   const container = createDOMElement('div');
+  container.className = 'question';
   const title = createDOMElement('h1');
   title.innerText = question.text;
   container.appendChild(title);
-
+  
+  // Create answers container 
   const answerContainer = createDOMElement('ol');
 
   for (const answerKey in question.answers) {
@@ -32,6 +40,22 @@ export const createQuestionElement = (question) => {
 
   container.appendChild(answerContainer);
 
+  // Create references container
+  const referencesContainer = createDOMElement('div');
+  referencesContainer.id = 'references-container';
+  
+  // Create references elements
+  const learnMore = createDOMElement('p');
+  learnMore.className = 'learn';
+  learnMore.innerText = 'Learn more: ' 
+  referencesContainer.appendChild(learnMore);
+
+  question.links.forEach(link => {
+    const theLink = createLinkElement(link.text);
+    theLink.href = link.href;
+    referencesContainer.appendChild(theLink);
+  })
+  container.appendChild(referencesContainer);
   return container;
 };
 
