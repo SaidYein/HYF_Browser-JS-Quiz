5a8e7473900849b7a1c60fb91a6f55de42811f70
Chris <fayadchristina@gmail.com>
2021-11-16 11:58:52 +0100

    Adding a Welcome Message & Iconic Horde vs.

--- src/init/initializeQuiz.js
+++ src/init/initializeQuiz.js
@@ -14,8 +14,12 @@ const initializeQuiz = () => {
 const showTheStartPage = () => {
   const userInterfaceContainer = getDOMElement('user-interface');
   const startPageContainer = createDOMElement('div', { id: 'startPage' });
+  const welcomeMessage = createDOMElement('h1', { className: 'welcome-message' });
+  welcomeMessage.innerText = 'Are You Up for a Challenge??';
+  startPageContainer.appendChild(welcomeMessage);
   const userNameElement = createDOMElement('input', { id: USER_NAME_ID });
   userNameElement.setAttribute('type', 'text');
+  userNameElement.placeholder = 'Type Your Name here..';
   startPageContainer.appendChild(userNameElement);
   const startTheQuizButton = createStartTheQuizButton();
   startPageContainer.appendChild(startTheQuizButton);
@@ -25,10 +29,8 @@ const showTheStartPage = () => {
     setupQuizHTML();
     showCurrentQuestion();
     showCurrentScore();
-    startTheQuizButton.style.visibility = "hidden";
+    startPageContainer.style.visibility = "hidden";
     userInterfaceContainer.style.backgroundImage = 'none';
-    const userName = document.getElementById(USER_NAME_ID).value;
-    quizData.userName = userName;
   }
   startTheQuizButton.addEventListener('click', startTheQuiz);
 }
@@ -37,6 +39,11 @@ const setupQuizHTML = () => {
   const userInterfaceContainer = getDOMElement('user-interface');
   const quizContainer = createDOMElement('div', { id: QUIZ_CONTAINER_ID });
   const currentQuestion = quizData.questions[quizData.currentQuestionIndex];
+  const teamsName = createDOMElement('h2', { className: 'teams-name' });
+  const userName = document.getElementById(USER_NAME_ID).value;
+  quizData.userName = userName;
+  teamsName.innerText = `Iconic Horde vs. ${quizData.userName}`;
+  quizContainer.appendChild(teamsName);
   quizContainer.appendChild(createStatusBarElement(quizData.currentTotalScore, currentQuestion.time));
   const questionContainer = createDOMElement('div', {
     id: QUESTION_CONTAINER_ID,
